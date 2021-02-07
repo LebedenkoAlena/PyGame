1abfc0a782913cd796408506aa85abc778185408
LebedenkoAlena <74965729+LebedenkoAlena@users.noreply.github.com>
2021-02-07 20:39:53 +0500

    Add files via upload

--- 1.py
+++ 1.py
@@ -3,7 +3,7 @@ import pygame, os, sys
 pygame.init()
 
 all_sprites = pygame.sprite.Group()
-size = HEIGHT, WIDTH = 700, 700
+size = HEIGHT, WIDTH = 800, 800
 STEP = 50
 screen = pygame.display.set_mode(size)
 pygame.display.set_caption('Перемещение героя')
@@ -33,12 +33,45 @@ def terminate():
     sys.exit()
 
 
+level_carrot = {
+    'leval_1.txt': 9,
+    'leval_2.txt': 13,
+    'leval_3.txt': 12,
+    'leval_4.txt': 35,
+    'leval_5.txt': 19,
+    'leval_6.txt': 24,
+    'leval_7.txt': 34,
+    'leval_8.txt': 8,
+    'leval_9.txt': 27,
+    'leval_10.txt': 17,
+    'leval_11.txt': 16,
+    'leval_12.txt': 27,
+    'leval_13.txt': 8,
+    'leval_14.txt': 17,
+    'leval_15.txt': 10,
+    'leval_16.txt': 21,
+    'leval_17.txt': 21,
+    'leval_18.txt': 18,
+    'leval_19.txt': 8,
+    'leval_20.txt': 23,
+    'leval_21.txt': 18,
+    'leval_22.txt': 18,
+    'leval_23.txt': 32,
+    'leval_24.txt': 11,
+    'leval_25.txt': 21,
+    'leval_26.txt': 65,
+    'leval_27.txt': 8,
+    'leval_28.txt': 22,
+    'leval_29.txt': 15,
+    'leval_30.txt': 10,
+}
+
 tile_images = {
     'carrot': load_image('carrot.png'),
     'box': load_image('box.png'),
     'earth': load_image('earth.png'),
     'end': load_image('end.png'),
-    'trap': load_image('trap1.png'),
+    'trap': load_image('trap.png'),
     'arrow_l': load_image('arrow_l.png'),
     'arrow_r': load_image('arrow_r.png'),
     'arrow_b': load_image('arrow_b.png'),
@@ -55,6 +88,11 @@ tile_images = {
     'key': load_image('key.png'),
 }
 player_image = load_image('rabbit.png')
+carrot_image = load_image('carrot.png')
+key_image = load_image('key.png')
+lock_image = load_image('lock.png')
+shetchik = load_image('shetchik1.png')
+ship_image = load_image('ship.png')
 
 tile_width = tile_height = 50
 # основной персонаж
@@ -63,7 +101,11 @@ player = None
 # группы спрайтов
 all_sprites = pygame.sprite.Group()
 tiles_group = pygame.sprite.Group()
+key_group = pygame.sprite.Group()
+lock_group = pygame.sprite.Group()
 player_group = pygame.sprite.Group()
+carrot_group = pygame.sprite.Group()
+ship_group = pygame.sprite.Group()
 
 
 class Tile(pygame.sprite.Sprite):
@@ -80,6 +122,57 @@ class Player(pygame.sprite.Sprite):
         self.image = player_image
         self.rect = self.image.get_rect().move(
             tile_width * pos_x - 14, tile_height * pos_y - 25)
+        self.radius = 5
+
+
+class Carrot(pygame.sprite.Sprite):
+    def __init__(self, pos_x, pos_y):
+        super().__init__(carrot_group, all_sprites)
+        self.image = carrot_image
+        self.rect = self.image.get_rect().move(
+            tile_width * pos_x, tile_height * pos_y)
+
+
+class Key(pygame.sprite.Sprite):
+    def __init__(self, pos_x, pos_y):
+        super().__init__(key_group, all_sprites)
+        self.image = key_image
+        self.rect = self.image.get_rect().move(
+            tile_width * pos_x, tile_height * pos_y)
+
+
+class Lock(pygame.sprite.Sprite):
+    def __init__(self, pos_x, pos_y):
+        super().__init__(lock_group, all_sprites)
+        self.image = lock_image
+        self.rect = self.image.get_rect().move(
+            tile_width * pos_x, tile_height * pos_y)
+        self.radius = 25
+
+
+class Ship(pygame.sprite.Sprite):
+    def __init__(self, pos_x, pos_y):
+        super().__init__(ship_group, all_sprites)
+        self.image = ship_image
+        self.rect = self.image.get_rect().move(
+            tile_width * pos_x, tile_height * pos_y)
+        self.radius = 25
+
+
+def draw_lives(surf, x, y, img):
+    img_rect = img.get_rect()
+    img_rect.x = x - 5
+    img_rect.y = y
+    surf.blit(img, img_rect)
+
+
+def draw_col(col, h):
+    font = pygame.font.Font(None, 90)
+    text = font.render(col, True, (255, 255, 255))
+    if int(col) < 10:
+        screen.blit(text, (h - 100, 5))
+    else:
+        screen.blit(text, (h - 130, 5))
 
 
 def generate_level(level):
@@ -98,9 +191,8 @@ def generate_level(level):
                 Tile('end', x, y)
             elif level[y][x] == 'm':
                 Tile('earth', x, y)
-                Tile('carrot', x, y)
+                Carrot(x, y)
             elif level[y][x] == '/':
-                Tile('earth', x, y)
                 Tile('trap', x, y)
             elif level[y][x] == '<':
                 Tile('earth', x, y)
@@ -140,10 +232,10 @@ def generate_level(level):
                 Tile('button_yes', x, y)
             elif level[y][x] == '%':
                 Tile('earth', x, y)
-                Tile('lock', x, y)
+                Lock(x, y)
             elif level[y][x] == '!':
                 Tile('earth', x, y)
-                Tile('key', x, y)
+                Key(x, y)
     # вернем игрока, а также размер поля в клетках
     return new_player, x, y
 
@@ -161,113 +253,157 @@ def load_level(filename):
     return list(map(lambda x: x.ljust(max_width, '.'), level_map))
 
 
-class Camera:
-    # зададим начальный сдвиг камеры
-    def __init__(self):
-        self.dx = 0
-        self.dy = 0
-
-    # сдвинуть объект obj на смещение камеры
-    def apply(self, obj):
-        obj.rect.x += self.dx
-        obj.rect.y += self.dy
-
-    # позиционировать камеру на объекте target
-    def update(self, target):
-        self.dx = -(target.rect.x + target.rect.w // 2 - WIDTH // 2)
-        self.dy = -(target.rect.y + target.rect.h // 2 - HEIGHT // 2)
-
-
-def start_screen():
-    intro_text = ["ЗАСТАВКА", "",
-                  "Правила игры",
-                  "Если в правилах несколько строк,",
-                  "приходится выводить их построчно"]
-
-    fon = pygame.transform.scale(load_image('fon.jpg'), (WIDTH, HEIGHT))
-    screen.blit(fon, (0, 0))
-    font = pygame.font.Font(None, 30)
-    text_coord = 50
-    for line in intro_text:
-        string_rendered = font.render(line, 1, pygame.Color('black'))
-        intro_rect = string_rendered.get_rect()
-        text_coord += 10
-        intro_rect.top = text_coord
-        intro_rect.x = 10
-        text_coord += intro_rect.height
-        screen.blit(string_rendered, intro_rect)
-
-    while True:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                terminate()
-            elif event.type == pygame.KEYDOWN or \
-                    event.type == pygame.MOUSEBUTTONDOWN:
-                return  # начинаем игру
-        pygame.display.flip()
-        clock.tick(FPS)
-
-
-def get_coords(level):
+def get_coords(level, object):
     for y in range(len(level)):
         for x in range(len(level[y])):
-            if level[y][x] == '@':
+            if level[y][x] == object:
                 return int(x), int(y)
 
 
-def moving(direction, level, x, y):
+def coords(*args):
+    return tile_width * args[0][0], tile_height * args[0][1]
+
+
+def game_over(screen):
+    screen.fill((0, 0, 0))
+    font = pygame.font.Font(None, 100)
+    text = font.render("GAME OVER", True, (136, 231, 252))
+    text_x = WIDTH // 2 - text.get_width() // 2
+    text_y = HEIGHT // 2 - text.get_height() // 2
+    text_w = text.get_width()
+    text_h = text.get_height()
+    screen.blit(text, (text_x, text_y))
+    pygame.draw.rect(screen, (136, 231, 252), (text_x - 10, text_y - 10,
+                                               text_w + 20, text_h + 20), 10)
+
+
+def moving(direction, level, x, y, col_key):
+    crossing_lock = pygame.sprite.spritecollide(player, lock_group, False, pygame.sprite.collide_circle)
     logic = True
-    if direction == 'right' and level[y][x + 1] == '0' \
-            or direction == 'left' and level[y][x - 1] == '0' \
-            or direction == 'down' and level[y + 1][x] == '0' \
-            or direction == 'up' and level[y - 1][x] == '0':
-        logic = False
-    elif direction == 'right' and level[y][x + 1] == '#' \
+    if direction == 'right' and level[y][x + 1] == '#' \
             or direction == 'left' and level[y][x - 1] == '#' \
             or direction == 'down' and level[y + 1][x] == '#' \
             or direction == 'up' and level[y - 1][x] == '#':
         logic = False
+    elif direction == 'right' and level[y][x + 1] == '0' \
+            or direction == 'left' and level[y][x - 1] == '0' \
+            or direction == 'down' and level[y + 1][x] == '0' \
+            or direction == 'up' and level[y - 1][x] == '0':
+        logic = False
+    elif direction == 'right' and level[y][x + 1] == '<' \
+            or direction == 'down' and level[y + 1][x] == '<' \
+            or direction == 'up' and level[y - 1][x] == '<' \
+            or direction == 'left' and level[y][x - 1] == '>' \
+            or direction == 'down' and level[y + 1][x] == '>' \
+            or direction == 'up' and level[y - 1][x] == '>' \
+            or direction == 'down' and level[y + 1][x] == ')' \
+            or direction == 'right' and level[y][x + 1] == ')' \
+            or direction == 'left' and level[y][x - 1] == ')' \
+            or direction == 'up' and level[y - 1][x] == '(' \
+            or direction == 'right' and level[y][x + 1] == '(' \
+            or direction == 'left' and level[y][x - 1] == '(':
+        logic = False
+    elif level[y][x] == '/':
+        Ship(x, y)
+    elif direction == 'right' and crossing_lock and col_key < 1 \
+            or direction == 'left' and crossing_lock and col_key < 1 \
+            or direction == 'down' and crossing_lock and col_key < 1 \
+            or direction == 'up' and crossing_lock and col_key < 1:
+        logic = False
+
     return logic
 
 
-level = load_level('leval_23.txt')
-player, level_x, level_y = generate_level(load_level('leval_23.txt'))
-camera = Camera()
-start_screen()
+class AnimatedSprite(pygame.sprite.Sprite):
+    def __init__(self, sheet, columns, rows, *args):
+        super().__init__(all_sprites)
+        self.frames = []
+        self.cut_sheet(sheet, columns, rows)
+        self.cur_frame = 0
+        self.image = self.frames[self.cur_frame]
+        x, y = args[0][0], args[0][1]
+        self.rect = self.rect.move(x, y)
+
+    def cut_sheet(self, sheet, columns, rows):
+        self.rect = pygame.Rect(0, 0, sheet.get_width() // columns,
+                                sheet.get_height() // rows)
+        for j in range(rows):
+            for i in range(columns):
+                frame_location = (self.rect.w * i, self.rect.h * j)
+                self.frames.append(sheet.subsurface(pygame.Rect(
+                    frame_location, self.rect.size)))
+
+    def update(self):
+        self.cur_frame = (self.cur_frame + 1) % len(self.frames)
+        self.image = self.frames[self.cur_frame]
+
+
+leval = 'leval_2.txt'
+level = load_level(leval)
+player, level_x, level_y = generate_level(level)
+col = level_carrot[leval]
+col_key = 0
+
+target = AnimatedSprite(load_image("animation.png"), 2, 1, coords(get_coords(level, '$')))
+fullname = os.path.join('data', "music.ogg")
+filepath = os.path.abspath(__file__)
+filedir = os.path.dirname(filepath)
+musicpath = os.path.join(filedir, fullname)
+pygame.mixer.music.load(musicpath)
+pygame.mixer.music.set_volume(0.4)
+pygame.mixer.music.play(loops=-1)
 
 running = True
-x, y = get_coords(level)
+x, y = get_coords(level, '@')
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_LEFT:
-                if moving('left', level, x, y):
+                if moving('left', level, x, y, col_key):
                     player.rect.x -= STEP
                     x -= 1
             if event.key == pygame.K_RIGHT:
-                if moving('right', level, x, y):
+                if moving('right', level, x, y, col_key):
                     player.rect.x += STEP
                     x += 1
             if event.key == pygame.K_UP:
-                if moving('up', level, x, y):
+                if moving('up', level, x, y, col_key):
                     player.rect.y -= STEP
                     y -= 1
-
             if event.key == pygame.K_DOWN:
-                if moving('down', level, x, y):
+                if moving('down', level, x, y, col_key):
                     player.rect.y += STEP
                     y += 1
-
-    camera.update(player)
-    for sprite in all_sprites:
-        camera.apply(sprite)
-
     fon = pygame.transform.scale(load_image('grass.jpg'), (WIDTH, HEIGHT))
     screen.blit(fon, (0, 0))
     tiles_group.draw(screen)
     player_group.draw(screen)
+    carrot_group.draw(screen)
+    key_group.draw(screen)
+    ship_group.draw(screen)
+    lock_group.draw(screen)
+    draw_lives(screen, WIDTH - 50, 5,
+               shetchik)
+
+    crossing_carrot = pygame.sprite.spritecollide(player, carrot_group, True, pygame.sprite.collide_circle)
+    crossing_ship = pygame.sprite.spritecollide(player, ship_group, False, pygame.sprite.collide_circle)
+    crossing_key = pygame.sprite.spritecollide(player, key_group, True, pygame.sprite.collide_circle)
+    if crossing_carrot:
+        col -= 1
+    draw_col(str(col), HEIGHT)
+    if crossing_ship:
+        game_over(screen)
+    if crossing_key:
+        col_key += 1
+    if col_key >= 1:
+        crossing_lock = pygame.sprite.spritecollide(player, lock_group, True, pygame.sprite.collide_circle)
+        if crossing_lock:
+            col_key -= 1
+    if col == 0:
+        all_sprites.draw(screen)
+        target.update()
 
     pygame.display.flip()
     clock.tick(FPS)
