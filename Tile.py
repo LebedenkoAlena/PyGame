455890d1549031bac4fb2d206f043c12cde43a28
Anastasia <n_vladimirceva@mail.ru>
2021-02-08 18:57:46 +0500

    связь QT с PyGame2,0

--- Tile.py
+++ Tile.py
@@ -16,6 +16,27 @@ class Game():
         self.x_pos = 15
         self.v = 10
         self.x = 1
+        self.tile_images = {
+            'carrot': self.load_image('carrot.png'),
+            'box': self.load_image('box.png'),
+            'earth': self.load_image('earth.png'),
+            'end': self.load_image('end.png'),
+            'trap': self.load_image('trap.png'),
+            'arrow_l': self.load_image('arrow_l.png'),
+            'arrow_r': self.load_image('arrow_r.png'),
+            'arrow_b': self.load_image('arrow_b.png'),
+            'arrow_t': self.load_image('arrow_t.png'),
+            'most=': self.load_image('most=.png'),
+            'most': self.load_image('most.png'),
+            '1': self.load_image('1.png'),
+            '2': self.load_image('2.png'),
+            '3': self.load_image('3.png'),
+            '4': self.load_image('4.png'),
+            'button_no': self.load_image('button_no.png'),
+            'button_yes': self.load_image('button_yes.png'),
+            'lock': self.load_image('lock.png'),
+            'key': self.load_image('key.png'),
+        }
         level_carrot = {
             'leval_1.txt': 9,
             'leval_2.txt': 13,
@@ -98,3 +119,78 @@ class Game():
             self.screen.blit(text, (h - 100, 5))
         else:
             self.screen.blit(text, (h - 130, 5))
+
+    def generate_level(level):
+        new_player, x, y = None, None, None
+        for y in range(len(level)):
+            for x in range(len(level[y])):
+                if level[y][x] == '.':
+                    Tile('earth', x, y)
+                elif level[y][x] == '#':
+                    Tile('box', x, y)
+                elif level[y][x] == '@':
+                    Tile('earth', x, y)
+                    new_player = Player(x, y)
+                elif level[y][x] == '$':
+                    Tile('earth', x, y)
+                    Tile('end', x, y)
+                elif level[y][x] == 'm':
+                    Tile('earth', x, y)
+                    Carrot(x, y)
+                elif level[y][x] == '/':
+                    Tile('trap', x, y)
+                elif level[y][x] == '<':
+                    Tile('earth', x, y)
+                    Tile('arrow_l', x, y)
+                elif level[y][x] == '>':
+                    Tile('earth', x, y)
+                    Tile('arrow_r', x, y)
+                elif level[y][x] == '(':
+                    Tile('earth', x, y)
+                    Tile('arrow_b', x, y)
+                elif level[y][x] == ')':
+                    Tile('earth', x, y)
+                    Tile('arrow_t', x, y)
+                elif level[y][x] == '-':
+                    Tile('earth', x, y)
+                    Tile('most=', x, y)
+                elif level[y][x] == '*':
+                    Tile('earth', x, y)
+                    Tile('most', x, y)
+                elif level[y][x] == '1':
+                    Tile('earth', x, y)
+                    Tile('1', x, y)
+                elif level[y][x] == '2':
+                    Tile('earth', x, y)
+                    Tile('2', x, y)
+                elif level[y][x] == '3':
+                    Tile('earth', x, y)
+                    Tile('3', x, y)
+                elif level[y][x] == '4':
+                    Tile('earth', x, y)
+                    Tile('4', x, y)
+                elif level[y][x] == '8':
+                    Tile('earth', x, y)
+                    Tile('button_no', x, y)
+                elif level[y][x] == '5':
+                    Tile('earth', x, y)
+                    Tile('button_yes', x, y)
+                elif level[y][x] == '%':
+                    Tile('earth', x, y)
+                    Lock(x, y)
+                elif level[y][x] == '!':
+                    Tile('earth', x, y)
+                    Key(x, y)
+        # вернем игрока, а также размер поля в клетках
+        return new_player, x, y
+
+
+class Tile(pygame.sprite.Sprite, Game):
+    def __init__(self, tile_type, pos_x, pos_y):
+        super().__init__(self.tiles_group, self.all_sprites)
+        self.image = self.tile_images[tile_type]
+        self.rect = self.image.get_rect().move(
+            self.tile_width * pos_x, self.tile_height * pos_y)
+
+
+
